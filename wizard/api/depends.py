089a342c750233f65fa051055f99ee19fe49a8d9
Lucien Shui <lucien@lucien.ink>
2025-04-07 19:14:33 +0800

    Remove db access, remove tasks api

--- wizard/api/depends.py
+++ wizard/api/depends.py
@@ -1,16 +1,9 @@
 from typing import Annotated
 
 from fastapi import Header
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from common.trace_info import TraceInfo
-from wizard.db import session_context
 
 
 def get_trace_info(x_trace_id: Annotated[str | None, Header()] = None) -> TraceInfo:
     return TraceInfo(trace_id=x_trace_id)
-
-
-async def get_session() -> AsyncSession:
-    async with session_context() as session:
-        yield session

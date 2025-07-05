f1bcf2e9340590a1839accb5b3e59f2f81dd703d
Lucien <lucien@lucien.ink>
2025-07-05 17:29:27 +0800

    Refact app factory

--- omnibox_wizard/wizard/api/server.py
+++ omnibox_wizard/wizard/api/server.py
@@ -27,19 +27,17 @@ async def exception_handler(_: Request, e: Exception) -> Response:
     return JSONResponse(status_code=500, content={"code": 500, "error": CommonException.parse_exception(e)})
 
 
-def app_factory(init_funcs: list[Callable[..., Awaitable]] | None = None, _version: str | None = None):
-    init_funcs = init_funcs or [init]
-    if _version is None:
-        with project_root.open("pyproject.toml", "rb") as f:
-            _version = tomllib.load(f)["project"]["version"]
-
+def app_factory(
+        init_funcs: list[Callable[..., Awaitable]] | None = None,
+        version: str | None = None
+) -> FastAPI:
     @asynccontextmanager
     async def lifespan(_: FastAPI):
         for init_func in init_funcs:
             await init_func()
         yield
 
-    _app = FastAPI(lifespan=lifespan, version=_version)
+    _app = FastAPI(lifespan=lifespan, version=version)
 
     _app.add_middleware(
         CORSMiddleware,  # noqa
@@ -51,9 +49,13 @@ def app_factory(init_funcs: list[Callable[..., Awaitable]] | None = None, _versi
 
     _app.add_exception_handler(Exception, exception_handler)
 
-    _app.include_router(v1_router, tags=["Wizard API"])
-    _app.include_router(internal_router, tags=["Internal API"])
     return _app
 
 
-app = app_factory()
+with project_root.open("pyproject.toml", "rb") as f:
+    project_version: str = tomllib.load(f)["project"]["version"]
+
+app: FastAPI = app_factory(init_funcs=[init], version=project_version)
+
+app.include_router(v1_router, tags=["Wizard API"])
+app.include_router(internal_router, tags=["Internal API"])

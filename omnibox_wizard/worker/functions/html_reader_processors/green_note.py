be12124c22107a2125ac6469926163273f47fb74
Lucien <lucien@lucien.ink>
2025-10-02 19:59:17 +0800

    chore(collect): Fix hit method

--- omnibox_wizard/worker/functions/html_reader_processors/green_note.py
+++ omnibox_wizard/worker/functions/html_reader_processors/green_note.py
@@ -5,6 +5,9 @@ from html2text import html2text
 
 from omnibox_wizard.worker.entity import GeneratedContent
 from omnibox_wizard.worker.functions.html_reader_processors.base import HTMLReaderBaseProcessor
+from opentelemetry import trace
+
+tracer = trace.get_tracer("GreenNoteProcessor")
 
 
 class GreenNoteProcessor(HTMLReaderBaseProcessor):
@@ -17,10 +20,11 @@ class GreenNoteProcessor(HTMLReaderBaseProcessor):
         if parsed.netloc == 'mp.weixin.qq.com':
             if parsed.path.startswith('/s/'):
                 soup = BeautifulSoup(html, "html.parser")
-                if soup.find("p", attrs={"id": "js_image_desc"}):
+                if soup.select("p#js_image_desc") or soup.select("div#js_image_content h1"):
                     return True
         return False
 
+    @tracer.start_as_current_span("GreenNoteProcessor.convert")
     async def convert(self, html: str, url: str) -> GeneratedContent:
         soup = BeautifulSoup(html, "html.parser")
         images = soup.select("div.swiper_item_img img:not(#img_item_placeholder)")

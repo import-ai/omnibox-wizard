0931120afe6055ee8a1609d0b1f66d852e327d4a
Lucien <lucien@lucien.ink>
2025-10-24 01:22:28 +0800

    fix(video): Fix audio extract stderr decode error

--- tests/omnibox_wizard/functions/test_single_task.py
+++ tests/omnibox_wizard/functions/test_single_task.py
@@ -7,11 +7,11 @@ from omnibox_wizard.worker.worker import Worker
 from tests.omnibox_wizard.helper.fixture import worker_config
 
 
-@pytest.mark.parametrize("task_id", ["UqZHmUr28DyZRQ4y5SxGji"])
+@pytest.mark.parametrize("task_id", [""])
 async def test_single_task(worker_config: WorkerConfig, task_id: str):
     worker = Worker(worker_config, worker_id=0)
     with httpx.Client(base_url=worker_config.backend.base_url) as client:
-        httpx_response: httpx.Response = client.get(f"/api/v1/tasks/{task_id}")
+        httpx_response: httpx.Response = client.get(f"/internal/api/v1/wizard/tasks/{task_id}")
         httpx_response.raise_for_status()
         json_response: dict = httpx_response.json()
         task = Task.model_validate(json_response)
